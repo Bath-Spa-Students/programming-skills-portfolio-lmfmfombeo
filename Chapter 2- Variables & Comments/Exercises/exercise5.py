9f2cc928eabff75b31d2421f2db39807b9c368ce
lmfmfombeo <mr03539@gmail.com>
2023-10-15 21:56:45 -0700

    exersice5.py

--- Chapter 2- Variables & Comments/Exercises/exercise5.py
+++ Chapter 2- Variables & Comments/Exercises/exercise5.py
@@ -1,2 +1,8 @@
 #A girl heads to a computer shop to buy some USB sticks. She loves USB sticks and wants as many as she can get for £50. They are £6 each.
 #Write a programme that calculates how many USB sticks she can buy and how many pounds she will have left.
+
+print("USB stick price £6.\n note: girl has £50.")
+quantity = int(input("how many can the girl buy: "))
+price = int (50 - (quantity * 6))
+print("the girl's change is: \n", price)
+
