be9a908b483a58203bd1c3b29519fef9e48598b4
lmfmfombeo <mr03539@gmail.com>
2023-10-20 09:18:17 -0700

    exercise.py
    chp3

---
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -0,0 +1,6 @@
+#Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples.
+
+favorite_transportation = ['car',"airplane"]
+
+print("I go to work using",(favorite_transportation[0]))
+print("I travel overseas using",(favorite_transportation[1]))
