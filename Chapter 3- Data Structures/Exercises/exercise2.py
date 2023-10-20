9d42fa5c879a149ae21ebfba321c44ecc90d702e
lmfmfombeo <mr03539@gmail.com>
2023-10-20 09:13:27 -0700

    exercise.py
    chp3

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,12 @@
+#Start with the list you used in Exercise 1, but instead of just
+
+#printing each person’s name, print a message to them. The text of each message should be the same, but each message should be 
+
+#personalized with the person’s name.
+
+names = ['abdullah','faisal','adil','abubaker','obaid']
+print("Hello, my good friend",(names[0]))
+print("Hello, my good friend",(names[1]))
+print("Hello, my good friend",(names[2]))
+print("Hello, my good friend",(names[3]))
+print("Hello, my good friend",(names[4]))
