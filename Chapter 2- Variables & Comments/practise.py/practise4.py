1cabd41a622b99b0fb364dc7d248fe7d251b9ad4
lmfmfombeo <mr03539@gmail.com>
2023-11-08 04:52:24 -0800

    .

---
+++ Chapter 2- Variables & Comments/practise.py/practise4.py
@@ -0,0 +1,9 @@
+#Write a python program that stores an integer and string value to variables x and y. Print the type of
+#variable x and y.
+
+x = 42
+y = "Hello, World!"
+
+# Print the type of variables x and y
+print("Type of x:", type(x))
+print("Type of y:", type(y))
---
+++ Chapter 2- Variables & Comments/practise.py/practise5.py
@@ -0,0 +1,14 @@
+#Write a python program that stores fruit names in a list named fruits. Unpack the list into three
+#variables x, y and z and then print the values of each variable.
+
+# Store fruit names in a list named "fruits"
+fruits = ["Apple", "Banana", "Cherry"]
+
+# Unpack the list into three variables
+x, y, z = fruits
+
+# Print the values of x, y, and z
+print("x:", x)
+print("y:", y)
+print("z:", z)
+
