52ad1a46495934a7aa18169b85446a81784218ed
lmfmfombeo <mr03539@gmail.com>
2023-11-13 06:30:45 -0800

    .

---
+++ Chapter 4- Control Flow/practice.py/practice1.py
@@ -0,0 +1,12 @@
+#Write a python program with if statement that assigns 20 to the variable y and assigns 40 to the
+#variable z if the variable & is greater than 100.
+
+variable = 110
+if variable > 100:
+    y = 40
+    z = 20
+    print("variable is grater than 100")
+    print("y=",y)
+    print("z=",z)
+else:
+    print("Variable & is not greater than 100.")
