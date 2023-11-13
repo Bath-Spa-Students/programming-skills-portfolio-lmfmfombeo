0ab4b315ce712dd3b7432ed5c51bba8b929146ee
lmfmfombeo <mr03539@gmail.com>
2023-11-12 22:34:36 -0800

    .

--- Chapter 3- Data Structures/practice.py/practice4.py
+++ Chapter 3- Data Structures/practice.py/practice4.py
@@ -1,13 +1,12 @@
-#. Assume the lists numbers1 has 100 elements and numbers2 is an empty list. Write code that
+#Assume the lists numbers1 has 100 elements and numbers2 is an empty list. Write code that
 #copies the values in numbers to numbers2.
 
-# Assuming numbers1 has 100 elements
-numbers1 = [1, 2, 3, ...]
-
-# Creating an empty list numbers2
+numbers1 = []
+for i in range (1,100):
+    numbers1.append(i)
 numbers2 = []
-
-# Copying the values from numbers1 to numbers2
-numbers2 = numbers1.copy()
-print(numbers2)
+print("before:",numbers2)
+numbers2=numbers1
+print("after:",numbers2)   
+   
 
