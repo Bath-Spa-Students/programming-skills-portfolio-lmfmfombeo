ee660e6ee74df8967b7a135ea689a2bf64f14402
lmfmfombeo <mr03539@gmail.com>
2023-11-14 08:23:53 -0800

    .

--- Chapter 4- Control Flow/practice.py/practice3.py
+++ Chapter 4- Control Flow/practice.py/practice3.py
@@ -1,6 +1,5 @@
 #Write a python program with nested decision structures that perform the following: If amount1
 #is greater than 10 and amount2 is less than 100, display the greater of amount1 and amount2
-
 amount1 = 14
 amount2 = 88
 
