129c7c666aff3fad99e3a11f65efd952bfe904cf
lmfmfombeo <mr03539@gmail.com>
2023-11-13 22:41:45 -0800

    .

---
+++ Chapter 7- Functions/practice/practice4.py
@@ -0,0 +1,11 @@
+#Write a Python program that defines a function to calculate the area of a circle, and then
+#calls that function with a given radius.
+
+import math
+
+def cal_circle_area(radius):
+    area = math.pi * radius**2
+    return area
+radius = float(input("enter the radius: "))
+area_of_circle = cal_circle_area(radius)
+print(f"area of circle with radius {radius} is: {area_of_circle}")
