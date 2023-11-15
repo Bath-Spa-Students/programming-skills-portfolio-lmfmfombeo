d9471daf2416f9b53c7cdd9801611061a5457cad
lmfmfombeo <mr03539@gmail.com>
2023-11-15 08:24:45 -0800

    .

--- Chapter 5- Dictionaries/practice/practice4.py
+++ Chapter 5- Dictionaries/practice/practice4.py
@@ -1,4 +1,5 @@
 #Write a Python program to iterate through both the keys and values of a dictionary and print them
+
 dict = {'a':"cat",'b':"bat",'c':"dog"}
 for key, value in dict.items():
     print(f"key: {key}, value: {value}")
