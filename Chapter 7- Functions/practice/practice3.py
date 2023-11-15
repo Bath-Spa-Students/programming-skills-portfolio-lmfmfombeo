8561e0037a4c6fe5be251d4c830a3c35990de0d4
lmfmfombeo <mr03539@gmail.com>
2023-11-15 08:02:26 -0800

    .

--- Chapter 7- Functions/practice/practice3.py
+++ Chapter 7- Functions/practice/practice3.py
@@ -1,14 +1,15 @@
 # Write a Python program that uses a function to check if a given number is prime or not
-def is_prime (no):
-    if no <= 1:
+def is_prime(num):
+    if num <= 1:
         return False
-    for i in range (2, int(no ** 0.5)+1):
-        if no % i == 0:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
             return False
-        return True
-input = int(input("enter the no if its prime: "))
-if is_prime(input):
-    print(f"{input} is prime no.")
+    return True
+# enter number
+number = int(input("Enter a number: "))
+
+if is_prime(number):
+    print(f"{number} is a prime number")
 else:
-    print(f"{input} is not prime no.")    
- 
+    print(f"{number} is not a prime number")
